9d3f18ba0f1e78efa3d27e1c1fa82492fd4aac3b
darkfader <none@none>
2013-12-31 18:10:28 +0100

    Internal handling of return status, fixes a bug

--- amanda/nagios-amanda-client.sh
+++ amanda/nagios-amanda-client.sh
@@ -20,6 +20,15 @@ amcommand="amcheck DailySet1 $backupclient"
 msg=""
 state=0
 
+
+max()
+{
+if [ $2 > 0 ] && [ $state -lt $2 ]; then
+   state=$2
+fi
+
+}
+
 # su test
 su - $amuser -c "cd ~${amuser} && true"
 if [ $? != 0 ]; then
@@ -35,13 +44,13 @@ ret=$?
 # check for broken client def
 if echo "$erg" | grep "matches neither a host nor a disk" > /dev/null ; then
    msg="$msg Client is not configured for backups"
-   state=$(( $state + 1 ))
+   max $state 2
 fi
 
 # check for client access issue
 if echo "$erg" | grep "ERROR: NAK" > /dev/null ; then
    msg="$msg Client is configured for backups, but they're not working."
-   state=$(( $state + 1 ))
+   max $state 2
 fi
 
 if echo "$erg" | grep "Client check: 1 host.* 0 problems" > /dev/null ; then
