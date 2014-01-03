cc3ff7f5165298699add7468ecfffa00288e4d8f
Florian Heigl <florian.heigl@gmail.com>
2014-01-03 17:12:39 +0100

    nagios-amanda-client.sh: Detect missing inital backup, Detect connection errors

--- amanda/nagios-amanda-client.sh
+++ amanda/nagios-amanda-client.sh
@@ -11,39 +11,56 @@ if [ $# != 1 ]; then
 fi
 
 # init vars
-shost=`echo $1 | cut -f1 -d\.`
-backupclient=${shost}.backup
 disabled=False
 
+# we have a dedicated backup subnet & hostnames
+# cut off the fqdn and append .backup
+# use backupclient=$1 if you dont need this.
+shost=`echo $1 | cut -f1 -d\.`
+backupclient=${shost}.backup
+
 amuser="amanda"
+# set name might also need to be configurable
+# send patch if you need it.
 amcommand="amcheck DailySet1 $backupclient"
 
 msg=""
 state=0
 
 
+# call to keep track of highest error level found
 max()
 {
 if [ $2 > 0 ] && [ $state -lt $2 ]; then
    state=$2
 fi
-
+# should a state higher than the ones nagios knows
+# be added, then we fudge it to "UNKNOWN" (3)
+if [ $2 -gt 3 ]; then
+   state=3
+fi
 }
 
-# su test
+
+# su test, to separate su und amanda errors
 su - $amuser -c "cd ~${amuser} && true"
 if [ $? != 0 ]; then
    echo "Cannot switch to amanda user, exiting."
    exit 3
 fi
 
+# run amanda selfcheck now and pick what it returns
 erg="`su - $amuser -c \"$amcommand\"`"
-# Return code not used atm
 ret=$?
 
+# let amandas return code influence overall result
+# (this catches unknown error strings)
+max $state $ret
+
+
+# check for broken client def
 check_def()
 {
-# check for broken client def
 # this needs to be check first since amanda returns ok for the client if it's not
 # even configured.
 if echo "$erg" | grep "matches neither a host nor a disk" > /dev/null ; then
@@ -53,15 +70,24 @@ if echo "$erg" | grep "matches neither a host nor a disk" > /dev/null ; then
 fi
 }
 
+
+# check for client access issues and broken backup.
 check_access()
 {
-# check for client access issue
 if echo "$erg" | grep "WARNING:.*selfcheck.*failed" > /dev/null ; then
-   msg="$msg Client is configured for backups, but Amanda failed to connect."
+   msg="$msg Amanda failed to connect"
    max $state 2
 fi
 if echo "$erg" | grep "ERROR: NAK" > /dev/null ; then
-   msg="$msg Client is configured for backups, but they're not working."
+   msg="$msg Amanda denies access"
+   max $state 2
+fi
+}
+
+check_index()
+{
+if echo "$erg" | grep "NOTE: index.*does not exist" | grep $backupclient > /dev/null; then
+   msg="$msg No initial backup for client."
    max $state 2
 fi
 }
@@ -70,7 +96,7 @@ fi
 check_ok()
 {
 # finally, check if the overall status is considered ok
-if echo "$erg" | grep "Client check: 1 host.* 0 problems" > /dev/null ; then
+if [ $state = 0 ] && echo "$erg" | grep "Client check: 1 host.* 0 problems" > /dev/null ; then
    msg="$msg Client is OK"
 fi
 }
@@ -84,6 +110,7 @@ check_def
 # speeds up stuff a lot
 if [ $disabled = "False" ]; then
   check_access
+  check_index
   check_ok
 fi
 
